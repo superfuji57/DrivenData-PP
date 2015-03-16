d33bcd32fcdc10ba6f5bed44a3160a8150efca2f
Andrew Won <andy.won@gmail.com>
2015-03-15 22:27:57 -0400

    update

--- exploratory.R
+++ exploratory.R
@@ -1,9 +1,17 @@
 # load the data
 # try hadley's new package
+library(readr)
 options(stringsAsFactors = FALSE)
 
 test <- read.csv("./data/test_values.csv")
+train <- read_csv("./data")
 head(test)
 dim(test)
 str(test)
 names(test)
+
+# feature definitions
+# n_ numerical; c_ categorical; o_ ordinal
+# release feature includes randomly ordered levels indicating to which cycle of the CDC data each row belongs
+# cycle means new data over time
+# columns beginning with service_ are the labels 
