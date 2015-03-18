7ab9065a4d98c1b3f998ee5aa7dd07851dac57c8
Andrew Won <andy.won@gmail.com>
2015-03-18 01:57:04 -0400

    update

--- exploratory.R
+++ exploratory.R
@@ -4,16 +4,19 @@ library(readr)
 options(stringsAsFactors = FALSE)
 
 test.values <- read.csv("./data/test_values.csv")
-train.values <- read_csv("./data/train_values.csv")
+train.values <- read.csv("./data/train_values.csv")
 train.labels <- read.csv("./data/train_labels.csv")
-
-head(test)
-dim(test)
-str(test)
-names(test)
 
 # feature definitions
 # n_ numerical; c_ categorical; o_ ordinal
 # release feature includes randomly ordered levels indicating to which cycle of the CDC data each row belongs
 # cycle means new data over time
 # columns beginning with service_ are the labels 
+
+names(train.values)
+na_count <- function(x){sum(is.na(x))}
+
+sapply(train.values, na_count)
+
+library(dplyr)
+summarise_each(train.values, funs(na_count))
